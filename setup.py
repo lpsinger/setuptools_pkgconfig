fd4f2f47100f017527c8f5623a4fa5e50e5f0b6b
Leo Singer <leo.singer@ligo.org>
2016-05-21 15:17:02 -0400

    Add classifiers

--- setup.py
+++ setup.py
@@ -8,6 +8,15 @@ setup(
     author_email='leo.singer@ligo.org',
     description=setuptools_pkgconfig.__doc__.splitlines()[1],
     long_description=setuptools_pkgconfig.__doc__,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: System :: Software Distribution'
+    ],
     py_modules=['setuptools_pkgconfig'],
     zip_safe=False,
     entry_points={
